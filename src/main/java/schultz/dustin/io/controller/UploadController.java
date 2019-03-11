e9110effabc633658b6f494f1d32eb4a647a5fd5
Rubén Below <rbelow@virteal.net>
2019-03-11 08:58:26 +0100

    first commit

---
+++ src/main/java/schultz/dustin/io/controller/UploadController.java
@@ -0,0 +1,35 @@
+package schultz.dustin.io.controller;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class UploadController {
+
+	private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup()
+			.lookupClass());
+
+	@Value("${multipart.location}") // multipart.location must be in application.properties
+	private String location;
+
+	// handle uploaded video and produce a gif
+	@RequestMapping(value = "/upload", method = RequestMethod.POST, produces =
+			MediaType.IMAGE_GIF_VALUE)
+	public String upload(@RequestPart("file") MultipartFile file,
+			@RequestParam("start") int start,
+			@RequestParam("end") int end,
+			@RequestParam("speed") int speed,
+			@RequestParam("repeat") boolean repeat) {
+		return "";
+	}
+}
