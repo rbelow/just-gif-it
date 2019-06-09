755729ac1183b38845cbb693e801857a31d4cc83
Rubén Below <rbelow@virteal.net>
2019-06-09 19:40:55 +0200

    angularjs app

---
+++ src/main/resources/public/bundle.js
@@ -0,0 +1,60 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	import 'bootstrap/dist/css/bootstrap.css';
+	import 'angular-loading-bar/build/loading-bar.css';
+	import './css/custom.css';
+	import angular from 'angular';
+	import angular_ui_router from 'angular-ui-router';
+	import angular_loading_bar from 'angular-loading-bar';
+	import config from './config';
+	import home from './home';
+	
+	angular.module('app', [angular_ui_router, angular_loading_bar, home]).config(config);
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
